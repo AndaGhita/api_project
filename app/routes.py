0230d96aa29221f704db1237a11f3aa3dbf1a97a
Anda Stefania Ghita <104030121+AndaGhita@users.noreply.github.com>
2025-11-26 15:50:25 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from __init__ import app #from the app module import app(__init__.py main)
+from app import __init__ 
 from user.models import User
 
 @app.route('/user/signup', methods=['GET'])
