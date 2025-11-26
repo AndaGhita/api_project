bc8f7769e9eb1f56c69391e0c6e509a7d82d504f
Anda Stefania Ghita <104030121+AndaGhita@users.noreply.github.com>
2025-11-26 15:49:26 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
+#list of notes
 notes = [
-    {"id": 1, "title": "Note 1", "content": "a"},
-    {"id": 2, "title": "Note 2", "content": "b"},
+    {"id": 1, "title": "Note 1", "content": "This is my first note", "created_at:": "2025-11-25"},
+    {"id": 2, "title": "Note 2", "content": "This is my second note", "created_at:" : "2025-11-26"}
 ]
 
